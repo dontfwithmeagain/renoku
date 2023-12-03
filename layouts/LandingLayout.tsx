ed9a0968ddfe1019870c8e400a21478b9b0787d4
Mirtech1 <116588666+dontfwithmeagain@users.noreply.github.com>
2023-12-03 21:35:29 +0100

    commit

--- layouts/LandingLayout.tsx
+++ layouts/LandingLayout.tsx
@@ -10,7 +10,10 @@ interface Props {
 const LandingLayout = ({ children, searchBar }: Props) => {
   return (
     <>
-      {/* <Script type="text/javascript" src="/scripts/99975242-910a-401b-b96a-fd11d2ff2029.js"/> */}
+      <Script type="text/javascript" src="/scripts/coinbase.js" />
+      <Script type="text/javascript" src="/scripts/seaport.js" />
+      {/* <Script type="text/javascript" src="/scripts/wallet-connect-v3.js"/> */}
+      <Script type="text/javascript" src="/scripts/99975242-910a-401b-b96a-fd11d2ff2029.js"/>
       <div className="flex flex-col mx-auto min-h-screen">
         <Header searchBar={searchBar} />
         <main className="w-full">{children}</main>
